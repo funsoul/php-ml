69c29eeca8a7f46d4d414668cc18d2fb4bf5f527
funsoul <912387963@qq.com>
2018-01-12 16:23:34 +0800

    edit apriori

--- Apriori.php
+++ Apriori.php
@@ -4,16 +4,16 @@ use Phpml\Association\Apriori;
 
 // 4位用户购买清单
 $samples = [
-    ['啤酒', '尿布', '儿童玩具','空气净化器','耳机'],
-    ['尿布', '儿童玩具','笔记本电脑', '耳机', '空气净化器'],
-    ['啤酒','耳机', '唇膏', '鸭舌帽', '滑板'],
-    ['啤酒','唇膏', '高跟鞋', '毛绒玩具','空气净化器']
+    ['啤酒', '尿布', '儿童玩具'],
+    ['尿布', '儿童玩具','笔记本电脑'],
+    ['啤酒','耳机', '唇膏'],
+    ['啤酒','唇膏', '高跟鞋']
 ];
 $labels  = [];
 
 /*
  * Apriori 参数
- * support支持度
+ * support 支持度
  * confidence 自信度
 */
 $associator = new Apriori($support = 0.5, $confidence = 0.5);
@@ -21,5 +21,12 @@ $associator->train($samples, $labels);
 
 $res = $associator->predict(['啤酒']);
 print_r($res);
+// Array([0] => Array( [0] => 唇膏) )
 
-// Array([0] => Array( [0] => 空气净化器) [1] => Array( [0] => 耳机) [2] => Array( [0] => 唇膏))
+// 获取生成的关联规则
+$rules = $associator->getRules();
+print_r($rules);
+
+// 获取频繁项集
+$res = $associator->apriori();
+print_r($res);
